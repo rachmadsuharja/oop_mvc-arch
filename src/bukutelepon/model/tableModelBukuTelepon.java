69bb4870e4e4fa127846b1e55fc1fda7b2c0c223
Harja <rachmadsuharja123@gmail.com>
2024-01-15 22:17:52 +0700

    Update tableModelBukuTelepon.java

--- src/bukutelepon/model/tableModelBukuTelepon.java
+++ src/bukutelepon/model/tableModelBukuTelepon.java
@@ -4,17 +4,59 @@
  */
 package bukutelepon.model;
 
-import java.awt.List;
-import javax.swing.table.*;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
 
 /**
  *
  * @author Harja
  */
 public class tableModelBukuTelepon extends AbstractTableModel {
-    List<bukutelepon> ib;
+    List<bukutelepon> lb;
     
     public tableModelBukuTelepon(List<bukutelepon> ib) {
-        this.ib = ib;
+        this.lb = lb;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return lb.size();
+    }
+
+    
+    public String getColumnNama(int column) {
+        switch (column) {
+            case 0:
+                return "ID";
+            case 1:
+                return "Nomer";
+            case 2:
+                return "Nama";
+            case 3:
+                return "Alamat";
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        switch (column) {
+            case 0:
+                return lb.get(row).getId();
+            case 1:
+                return lb.get(row).getNomer();
+            case 2:
+                return lb.get(row).getNama();
+            case 3:
+                return lb.get(row).getAlamat();
+            default:
+                return null;
+        }
     }
 }
